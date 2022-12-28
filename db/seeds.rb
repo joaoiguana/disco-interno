4851d8370a6f70c935b15b89973b0703012b3785
joao garcia <jiboiadamor@gmail.com>
2022-12-28 16:57:54 +0000

    added 10 tracks to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,15 @@ iguana_garcia = Artist.create!(name: 'Iguana Garcia', bio: "Iguana Garcia é um 
 
 # ALBUMS
 # Ilha Da Iguana
-Album.create!(title: 'Ilha Da Iguana', year:2021, description: "'Ilha Da Iguana' é o terceiro álbum de originais de Iguana Garcia. Bloqueado geograficamente, o mar do artista lisboeta só podia ser um. Embarcou com uma tripulação de MIDIs piratas, a drum machine como primeiro imediata, poeta na linguagem dos 0s e 1s. O objectivo era encontrar as correntes do house e do techno, não abandonando a pop, e ver que lugar terá a língua de Camões em tais latitudes. A guitarra ficou em terra. São 10 cantigas que contam o naufrágio de 2020 e 2021 para a música de dança, de olhos posto em futuras pistas.", genre: 'Indie-House', catalog_number: 'DILP002', artist: iguana_garcia)
+ilha_da_iguana = Album.create!(title: 'Ilha Da Iguana', year:2021, description: "'Ilha Da Iguana' é o terceiro álbum de originais de Iguana Garcia. Bloqueado geograficamente, o mar do artista lisboeta só podia ser um. Embarcou com uma tripulação de MIDIs piratas, a drum machine como primeiro imediata, poeta na linguagem dos 0s e 1s. O objectivo era encontrar as correntes do house e do techno, não abandonando a pop, e ver que lugar terá a língua de Camões em tais latitudes. A guitarra ficou em terra. São 10 cantigas que contam o naufrágio de 2020 e 2021 para a música de dança, de olhos posto em futuras pistas.", genre: 'Indie-House', catalog_number: 'DILP002', artist: iguana_garcia)
 # Ilha Da Iguana - TRACKS
+Track.create!(title: 'Ilha Da Iguana', tracks_number: 1, artist: iguana_garcia, album: ilha_da_iguana)
+Track.create!(title: 'Monotonia', tracks_number: 2, artist: iguana_garcia, album: ilha_da_iguana)
+Track.create!(title: 'Tarraxo do Animal Doméstico', tracks_number: 3, artist: iguana_garcia, album: ilha_da_iguana)
+Track.create!(title: 'Sunset Drive', tracks_number: 4, artist: iguana_garcia, album: ilha_da_iguana)
+Track.create!(title: 'Saudades', tracks_number: 5, artist: iguana_garcia, album: ilha_da_iguana)
+Track.create!(title: 'Dança da Lua', tracks_number: 6, artist: iguana_garcia, album: ilha_da_iguana)
+Track.create!(title: 'Fixação', tracks_number: 7, artist: iguana_garcia, album: ilha_da_iguana)
+Track.create!(title: 'Matrix da Pátria', tracks_number: 8, artist: iguana_garcia, album: ilha_da_iguana)
+Track.create!(title: 'A Gruta', tracks_number: 9, artist: iguana_garcia, album: ilha_da_iguana)
+Track.create!(title: 'Bolero Digital', tracks_number: 10, artist: iguana_garcia, album: ilha_da_iguana)
