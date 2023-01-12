d01b9e8bf0fa4092d5b2361cea17d0516ec3f630
joao garcia <jiboiadamor@gmail.com>
2023-01-12 15:50:53 +0000

    created click event for audio player to change track current time

--- client/src/components/AudioPlayer/AudioPlayer.jsx
+++ client/src/components/AudioPlayer/AudioPlayer.jsx
@@ -3,9 +3,8 @@ import './AudioPlayer.css';
 
 const AudioPlayer = ({ audioUrl }) => {
   const audioElement = useRef(null);
+  const progressBar = useRef();
   const audioElements = document.getElementsByTagName('audio');
-  const progress = document.querySelector('.progress');
-  const progressContainer = document.querySelector('.progress-container');
 
   const play = () => {
     for (let i = 0; i < audioElements.length; i++) {
@@ -18,28 +17,21 @@ const AudioPlayer = ({ audioUrl }) => {
     audioElement.current.pause();
   };
 
-  function updateProgress(e) {
-    const {duration, currentTime} = e.srcElement;
-    const progressPercent = (currentTime / duration) * 100;
-    progress.style.width = `${progressPercent}%`;
-  };
-
   const setProgress = (e) => {
-    const width = e.clientWidth;
-    const clickX = e.offsetX;
-    const duration = audioElement.duration
+    const clickX = e.nativeEvent.offsetX;
+    const width = progressBar.current.offsetWidth;
+    const duration = audioElement.current.duration;
 
-    audioElements.currentTime = (clickX / width) * duration;
-    console.log(audioElements.currentTime);
-  };
+    audioElement.current.currentTime = (clickX / width) * duration;
+  }
 
   return (
     <div className='details-player'>
       <audio ref={audioElement} src={audioUrl} />
       <button onClick={play} className='button-play'><i className="uil uil-play"></i></button>
       <button onClick={pause} className="button-previous-next"><i className="uil uil-pause"></i></button>
-      <div class="progress-container" onClick={setProgress}>
-        <div class="progress"></div>
+      <div className="progress-container" ref={progressBar} onClick={setProgress}>
+        <div className="progress"></div>
       </div>
     </div>
   );
