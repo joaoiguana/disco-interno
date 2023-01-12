9b3010acc44d63b3fa77d232545ab2ccfebda850
joao garcia <jiboiadamor@gmail.com>
2023-01-12 11:14:44 +0000

    started to add the progress bar code in the audioplayer component

--- client/src/components/AudioPlayer/AudioPlayer.jsx
+++ client/src/components/AudioPlayer/AudioPlayer.jsx
@@ -16,11 +16,22 @@ const AudioPlayer = ({ audioUrl }) => {
     audioElement.current.pause();
   };
 
+  const setProgress = (e) => {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audioElement.duration
+
+    audioElement.currentTime = (clickX / width) * duration;
+  }
+
   return (
     <div className='details-player'>
       <audio ref={audioElement} src={audioUrl} />
       <button onClick={play} className='button-play'><i className="uil uil-play"></i></button>
       <button onClick={pause} className="button-previous-next"><i className="uil uil-pause"></i></button>
+      <div class="progress-container" onClick={setProgress}>
+        <div class="progress"></div>
+      </div>
     </div>
   );
 };
