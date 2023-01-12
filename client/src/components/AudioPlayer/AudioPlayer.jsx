5b8096f92b30d59672ca6970ce0ad0165c3913cc
joao garcia <jiboiadamor@gmail.com>
2023-01-12 16:28:22 +0000

    added onTimeUpdate event for progressUpdate

--- client/src/components/AudioPlayer/AudioPlayer.jsx
+++ client/src/components/AudioPlayer/AudioPlayer.jsx
@@ -1,9 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import './AudioPlayer.css';
 
 const AudioPlayer = ({ audioUrl }) => {
+  const [currentTime, setCurrentTime] = useState(0);
   const audioElement = useRef(null);
   const progressBar = useRef();
+  const progress = useRef();
   const audioElements = document.getElementsByTagName('audio');
 
   const play = () => {
@@ -17,6 +19,15 @@ const AudioPlayer = ({ audioUrl }) => {
     audioElement.current.pause();
   };
 
+  const updateProgress = (e) => {
+    console.log(e.nativeEvent.srcElement)
+    const duration = e.nativeEvent.srcElement.duration;
+    const currentTime = e.nativeEvent.srcElement.currentTime;
+    const progressPercent = (currentTime / duration) * 100;
+
+    progress.current.style.width = `${progressPercent}%`
+  }
+
   const setProgress = (e) => {
     const clickX = e.nativeEvent.offsetX;
     const width = progressBar.current.offsetWidth;
@@ -27,11 +38,11 @@ const AudioPlayer = ({ audioUrl }) => {
 
   return (
     <div className='details-player'>
-      <audio ref={audioElement} src={audioUrl} />
+      <audio ref={audioElement} src={audioUrl} onTimeUpdate={updateProgress} />
       <button onClick={play} className='button-play'><i className="uil uil-play"></i></button>
       <button onClick={pause} className="button-previous-next"><i className="uil uil-pause"></i></button>
       <div className="progress-container" ref={progressBar} onClick={setProgress}>
-        <div className="progress"></div>
+        <div className="progress" ref={progress}></div>
       </div>
     </div>
   );
