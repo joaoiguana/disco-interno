9a4ce4b8a9e9eec3813bb912e6ca83fc9b9cc2ca
joao garcia <jiboiadamor@gmail.com>
2023-01-16 18:05:48 +0000

    worked on footer elements

--- client/src/components/Home/HomeFooter.jsx
+++ client/src/components/Home/HomeFooter.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const HomeFooter = () => {
   return (
     <div className='home-footer'>
+      <div className='footer-links'>
+        <p className='link-home'><a href="https://discointerno.bandcamp.com/" target="_blank"><i class="fa-brands fa-bandcamp"></i> Bandacamp</a></p>
+        <p className='link-home'><a href="https://www.instagram.com/discointerno/" target="_blank"><i class="fa-brands fa-instagram"></i> Instagram</a></p>
+      </div>
       <p><i className="fa-regular fa-copyright"></i> Direitos reservados aos artistas e Disco Interno.</p>
+      <p className='home-mail'><i class="fa-regular fa-envelope"></i> discointernolx@gmail.com</p>
     </div>
   );
 }
