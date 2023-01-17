48e33385b8e1168fbffe0c697685f2ac4c4f6e60
joao garcia <jiboiadamor@gmail.com>
2023-01-17 14:26:57 +0000

    removed icons from navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
   return (
     <nav>
       <Link exact to="/"><img src="https://res.cloudinary.com/doa9kh79y/image/upload/v1672756012/disco%20interno/logo/Logo-1366x768px_j9uybj.png" alt="logo-discointerno"  className='logo nav-logo'/></Link>
-      <Link to="/albums" className='nav-menus'><i className="uil uil-music"></i> Lançamentos</Link>
-      <Link to="/artists" className='nav-menus nav-artists'><i className="uil uil-boombox"></i> Artistas</Link>
+      <Link to="/albums" className='nav-menus'><strong>Lançamentos</strong></Link>
+      <Link to="/artists" className='nav-menus nav-artists'><strong>Artistas</strong></Link>
       {/* <Link to="/tracks">Tracks</Link> */}
     </nav>
   )
