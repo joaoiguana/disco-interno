ee166a8a0ff924ecf69e63a9850242d26bc77994
joao garcia <jiboiadamor@gmail.com>
2023-01-18 14:27:35 +0000

    adding style and information to about page

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
   return (
-    <div>
-
+    <div className='about-page'>
+      <div className="about-container">
+        <h2 className='margin-reset'>A Disco Interno é uma promessa de jazz.</h2>
+        <p>
+          Somos uma editora discográfica independente sediada em Lisboa, Portugal.
+          <br />
+          Apaixonados por todas as estéticas lounge, do modal ao acid jazz, passando por bossas de elevador ou universos
+          de latitudes house, fazemos nossa a permissa de apoiar e editar novos criativos portugueses que se revejam
+          nesta promessa.
+        </p>
+        <p>Se sentires que esta label pode ser tua, envia-nos a tua música para: <strong>discointernolx@gmail.com</strong></p>
+      </div>
     </div>
   );
 }
