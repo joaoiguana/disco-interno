ec26d3fd7c7914fc5e7ab8f789fc9b24132679dc
joao garcia <jiboiadamor@gmail.com>
2023-01-19 15:40:30 +0000

    added some italic to the text in the about page

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -10,8 +10,8 @@ const About = () => {
         <p>
           Somos uma editora discográfica independente sediada em Lisboa, Portugal.
           <br />
-          Apaixonados por todas as estéticas lounge, do modal ao acid jazz, passando por bossas de elevador ou universos
-          de latitudes house, fazemos nossa a permissa de apoiar e editar novos criativos portugueses que se revejam
+          Apaixonados por todas as estéticas <em>lounge</em>, do <em>modal</em> ao <em>acid jazz</em>, passando por <em>bossas</em> de elevador ou universos
+          de latitudes <em>house</em>, fazemos nossa a permissa de apoiar e editar novos criativos portugueses que se revejam
           nesta promessa.
         </p>
         <p>Se sentires que esta label pode ser tua, envia-nos a tua música para: <strong>discointernolx@gmail.com</strong></p>
