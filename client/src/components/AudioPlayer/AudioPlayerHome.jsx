b9e527e4fde28c3b4d9c2abbc2cb04cd696bc460
joao garcia <jiboiadamor@gmail.com>
2023-01-19 13:57:52 +0000

    Studied text placement for home page

--- client/src/components/AudioPlayer/AudioPlayerHome.jsx
+++ client/src/components/AudioPlayer/AudioPlayerHome.jsx
@@ -46,7 +46,7 @@ const AudioPlayerHome = () => {
 
   return (
     <div>
-      <button onClick={() => {toggleClass(); handleRandomClick()}} className='broadcast-button'><h2><i className="fa-solid fa-podcast"></i></h2></button>
+      <button onClick={() => {toggleClass(); handleRandomClick()}} className='broadcast-button'><p><i className="fa-solid fa-podcast"></i></p></button>
       <div className={isActive ? 'card': 'card hidden'} >
         <div className="music-widget">
           <img src={oneTrack.photo_url} alt="album_cover" className='art'/>
