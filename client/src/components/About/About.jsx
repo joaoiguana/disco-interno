90705d5c423c80ebe036dc5b47769e35eb6cc353
joao garcia <jiboiadamor@gmail.com>
2023-01-20 12:17:04 +0000

    studying design alternatives

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import BackButton from '../BackButton/BackButton';
+import HomeFooter from '../Home/HomeFooter';
 import './About.css';
 
 const About = () => {
   return (
     <div className='about-page'>
       <div className="about-container">
-        <h2 className='margin-reset'>A Disco Interno é uma promessa de jazz.</h2>
-        <p>
-          Somos uma editora discográfica independente sediada em Lisboa, Portugal.
-          <br />
-          Apaixonados por todas as estéticas <em>lounge</em>, do <em>modal</em> ao <em>acid jazz</em>, passando por <em>bossas</em> de elevador ou universos
-          de latitudes <em>house</em>, fazemos nossa a permissa de apoiar e editar novos criativos portugueses que se revejam
-          nesta promessa.
-        </p>
-        <p>Se sentires que esta label pode ser tua, envia-nos a tua música para: <strong>discointernolx@gmail.com</strong></p>
-        <BackButton />
+        <div className='about-text'>
+          <h2 className='margin-reset'>A Disco Interno é uma promessa de jazz.</h2>
+          <p>
+            Somos uma editora discográfica independente sediada em Lisboa, Portugal.
+            <br />
+            Apaixonados por todas as estéticas <em>lounge</em>, do <em>modal</em> ao <em>acid jazz</em>, passando por <em>bossas</em> de elevador ou universos
+            de latitudes <em>house</em>, fazemos nossa a permissa de apoiar e editar novos criativos portugueses que se revejam
+            nesta promessa.
+          </p>
+          <p>Se sentires que esta label pode ser tua, envia-nos a tua música para: <strong>discointernolx@gmail.com</strong></p>
+          <BackButton />
+        </div>
       </div>
+      <HomeFooter />
     </div>
   );
 }
